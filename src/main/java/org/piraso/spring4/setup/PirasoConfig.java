340d953405c2900f2e4f03f0ab6d633be0f997e0
adeleon <alvinrdeleon@me.com>
2016-09-09 14:42:11 +0800

    added spring4setup

---
+++ src/main/java/org/piraso/spring4/setup/PirasoConfig.java
@@ -0,0 +1,44 @@
+package org.piraso.spring4.setup;
+
+import org.piraso.server.spring.web.PirasoServlet;
+import org.piraso.web.base.PirasoFilter;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.context.support.HttpRequestHandlerServlet;
+
+import java.util.Collections;
+
+@Configuration
+@ImportResource("classpath*:spring/piraso.xml")
+public class PirasoConfig {
+
+    @Bean
+    public FilterRegistrationBean pirasoFilter() {
+        FilterRegistrationBean bean = new FilterRegistrationBean();
+        bean.setUrlPatterns(Collections.singleton("/*"));
+        bean.setFilter(new PirasoFilter());
+        bean.setOrder(Integer.MAX_VALUE);
+
+        return bean;
+    }
+
+    @Bean
+    public PirasoServlet pirasoServlet() {
+        return new PirasoServlet();
+    }
+
+    @Bean
+    public ServletRegistrationBean pirasoRequestHandlerServlet() {
+        ServletRegistrationBean bean = new ServletRegistrationBean();
+
+        HttpRequestHandlerServlet dispatcherServlet = new HttpRequestHandlerServlet();
+        bean.setName("pirasoServlet");
+        bean.setServlet(dispatcherServlet);
+        bean.setUrlMappings(Collections.singleton("/piraso/logging"));
+
+        return bean;
+    }
+}
